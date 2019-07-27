f6789dee4ae1e64645320bc2aa397495495f684c
AceIsHuman <47653241+AceIsHuman@users.noreply.github.com>
2019-07-27 13:44:27 -0500

    specials button bg color

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const SpecialButton = (props) => {
   return (
-    <button>
+    <button style={{backgroundColor: "#225d9e"}}>
       {/* Display a button element rendering the data being passed down from the parent container on props */
       props.special
       }
