e8c792251ef40447d74ba0c78b25c7f396d3813e
AceIsHuman <47653241+AceIsHuman@users.noreply.github.com>
2019-07-27 13:49:08 -0500

    operators button bg color

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const OperatorButton = (props) => {
   return (
-    <button>
+    <button style={{background: "#247293"}}>
       {/* Display a button element rendering the data being passed down from the parent container on props */
       props.operatorChar}
     </button>
