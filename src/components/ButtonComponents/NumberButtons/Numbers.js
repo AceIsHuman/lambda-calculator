860160bec6eec7384f39ad6492224419a7c67893
AceIsHuman <47653241+AceIsHuman@users.noreply.github.com>
2019-07-27 12:16:44 -0500

    create numbers container

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 
 //import any components needed
+import NumberButton from "./NumberButton";
 
 //Import your array data to from the provided data file
 import data from "../../../data";
@@ -10,10 +11,16 @@ const Numbers = () => {
   // STEP 2 - add the imported data to state
   const [numberState, setNumberState] = useState(numbers);
   return (
-    <div>
+    <div className="numbers-container">
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+       it any props needed by the child component*/
+      numbers.map(num => {
+        return (
+          <NumberButton num={num} />
+        )
+      })
+      }
     </div>
   );
 };
