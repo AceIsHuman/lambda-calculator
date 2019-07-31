99e4f70362af54c1971bb2303a0116219ada1412
AceIsHuman <47653241+AceIsHuman@users.noreply.github.com>
2019-07-30 22:06:04 -0500

    setInput on button click

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const NumberButton = (props) => {
   return (
-    <button style={{background: "#184379"}}>
+    <button style={{background: "#184379"}} onClick={() => props.setInput(props.num)}>
       {props.num}
     </button>
   );
