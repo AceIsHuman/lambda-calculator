d713b89b8f040be40026c9f1cd07201154229a1b
AceIsHuman <47653241+AceIsHuman@users.noreply.github.com>
2019-07-30 22:03:59 -0500

    create state for input

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 // STEP 4 - import the button and display components
 
@@ -13,6 +13,7 @@ import Logo from "./components/DisplayComponents/Logo";
 import Display from "./components/DisplayComponents/Display";
 
 function App() {
+  const [input, setInput] = useState(0);
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
   // Once the state hooks are in place write some functions to hold data in state and update that data depending on what it needs to be doing
   // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
@@ -24,11 +25,11 @@ function App() {
       <Logo />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
-        <Display />
+        <Display input={input}/>
         <div className="buttons-container">
           <div className="buttons-left">
             <Specials />
-            <Numbers />
+            <Numbers setInput={setInput} />
           </div>
           <div className="buttons-right">
             <Operators />
